fa5993d648739428edd291b387f9a82afefa38e8
Yurii <oniks9821@gmail.com>
2024-12-03 16:25:15 +0200

    1625

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import userData from '../userData.json';
+import Profile from './profile/Profile';
+
+const App = () => {
+  return (
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+  );
+};
+
+export default App;
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+function Profile({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) {
+  const defaultImage =
+    'https://cdn-icons-png.flaticon.com/512/1077/1077012.png';
+
+  return (
+    <div className={css.profile}>
+      <div className={css.imageContainer}>
+        <img
+          src={image || defaultImage} // Если image не передан или равен null/undefined
+          alt="User avatar"
+          className={css.image}
+        />
+        <p className={css.name}>{name}</p>
+        <p className={css.dscr}>@{tag}</p>
+        <p className={css.dscr}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.item}>
+          <span>Followers</span>
+          <span className={css.data}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span>Views</span>
+          <span className={css.data}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span>Likes</span>
+          <span className={css.data}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
